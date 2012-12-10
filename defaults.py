c86270defd82bfefe916eeab0fc6ea33c5883a94
Chris Ravenscroft <github@voilaweb.com>
2012-12-10 03:24:28 +0300

    Inital check in

---
+++ defaults.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from mezzanine.conf import register_setting
+
+# Did you know? If you forget to import settings, above, then, well this works...until these settings disappear!
+# Also, do not panic if settings disappear when a dev server is shut down: they only seem to be persisted when
+# in production.
+
+register_setting(
+    name="MOLLOM_PUBLIC_KEY",
+    label=_("Mollom API Public Key"),
+    description=_("API Public Key"),
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="MOLLOM_PRIVATE_KEY",
+    label=_("Mollom API Private Key"),
+    description=_("API Private Key"),
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="MOLLOM_DEFAULT_SERVER",
+    label=_("Mollom Default Server"),
+    description=_("Only enter a value if Mollom is unable to talk to its default server. Typically, this default value is 'xmlrpc.mollom.com' but if that server is unresponsive, you may try, for instance, 'xmlrpc3.mollom.com'"),
+    editable=True,
+    default="",
+)
+
---
+++ filters.py
@@ -0,0 +1,47 @@
+import logging
+from mezzanine.utils import views
+from mezzanine.conf import settings
+from django.forms import Textarea
+from Mollom import MollomAPI
+from Mollom import MollomFault
+
+def is_spam_mollom(request, form, url):
+    def logit(msg):
+        if settings.DEBUG:
+            logger = logging.getLogger(__name__)
+            logger.info(msg)
+
+
+    settings.use_editable() # Re-eval setting every time
+    if not settings.MOLLOM_PUBLIC_KEY or not settings.MOLLOM_PRIVATE_KEY:
+        return False
+    else:
+        comment = None
+        for name, field in form.fields.items():
+            if isinstance(field.widget, Textarea):
+                comment = form.cleaned_data.get(name)
+                break
+        if comment == None:
+            return False
+        else:
+            if not settings.MOLLOM_DEFAULT_SERVER:
+                mollom_api = MollomAPI(
+                    publicKey=str(settings.MOLLOM_PUBLIC_KEY),
+                    privateKey=str(settings.MOLLOM_PRIVATE_KEY))
+            else:
+                mollom_api = MollomAPI(
+                    publicKey=str(settings.MOLLOM_PUBLIC_KEY),
+                    privateKey=str(settings.MOLLOM_PRIVATE_KEY),
+                    defaultServer="http://%s" % settings.MOLLOM_DEFAULT_SERVER)
+            if not mollom_api.verifyKey():
+                raise MollomFault('Your MOLLOM credentials are invalid.')
+            cc = mollom_api.checkContent(postBody=comment)
+            if cc['spam'] == 2:
+                logit("MOLLOM::SPAM: %s" % comment)
+                return True
+            elif cc['spam'] == 3:
+                logit("MOLLOM::not sure: %s" % comment)
+                return False
+            else:
+                logit("MOLLOM::ham: %s" % comment)
+                return False
